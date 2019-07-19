a8fd2d8de0908f8d8ec679a9d360513c39fa1448
Kintaro-Oe <marcusclairmont@gmail.com>
2019-07-19 13:22:43 +0100

    refactor to receive external files
    add #try_to_load
    need to fix test coverage of this method

--- lib/log_parse.rb
+++ lib/log_parse.rb
@@ -1,5 +1,31 @@
 class LogParse
-  def count_all_visits(log_file)
-    {"/help_page/1":1}
+  def initialize
+    @all_visits = {}
+  end
+
+  def count_all_visits(filename)
+    file = File.open(filename, "r")
+
+    file.readlines.each do |line|
+      visit_details = line.split(" ")
+      @all_visits[visit_details[0]] = 1
+    end
+    file.close
+
+    @all_visits
+  end
+
+  def try_load_log
+    filename = ARGV.first  # first argument from the command line
+    return if filename.nil? # get out of the method if it isn't given
+    if File.exists?(filename)
+      puts "Loaded from file #{filename}"
+      count_all_visits(filename)
+    else
+     puts "sorry, #{filename} doesn't exist."
+    end
   end
 end
+
+log_parse = LogParse.new
+log_parse.try_load_log
--- spec/log_parse_spec.rb
+++ spec/log_parse_spec.rb
@@ -3,9 +3,23 @@ require 'log_parse'
 describe LogParse do
   subject(:log_parse) { described_class.new }
 
+  describe '#try_load_log' do
+    it 'escapes from program if no file is given' do
+      expect(log_parse.try_load_log).to eq(nil)
+    end
+    # it 'returns message as to whether file given is successfully loaded' do
+    #
+    # end
+  end
+
   describe '#count_all_visits' do
     it 'returns a hash with the number of visits for each page' do
-      expect(log_parse.count_all_visits("/help_page/1 126.318.035.038")).to eq({"/help_page/1":1})
+      file = double(file)
+      allow(File).to receive(:open).with("sample.log", "r").and_return(file)
+      allow(file).to receive(:readlines).and_return(["/help_page/1 126.318.035.038"])
+      allow(file).to receive(:close)
+
+      expect(log_parse.count_all_visits("sample.log")).to eq({"/help_page/1" =>1})
     end
   end
 end
